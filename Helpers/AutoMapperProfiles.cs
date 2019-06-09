cd1aa4029f62f49da97d43deb19dd69f3d2fae32
georgesmhanna <georgesmhanna92@gmail.com>
2019-06-09 15:33:25 +0300

    Added users and photos DTOs Added and Configured Automapper Added DatingRepository Seeded data

---
+++ Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using DatingApp.API.Controllers.DTOs;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserForListDto>();
+            CreateMap<User, UserForDetailedDto>();
+            CreateMap<Photo, PhotoForDetailedDto>();
+        }
+    }
+}
